0618aa05e952a6a6ae4c224cb8dfcd6c147d2756
1354383179@qq.com <1354383179@qq.com>
2022-09-06 11:31:13 +0800

    faet: 编辑器组件错误时自动尝试刷新

--- src/components/Editor/components/ErrorComp.tsx
+++ src/components/Editor/components/ErrorComp.tsx
@@ -1,7 +1,20 @@
-import { Alert, Button } from "@arco-design/web-react";
-import { IconExclamationCircle } from "@arco-design/web-react/icon";
+import { Alert, Button, Space } from "@arco-design/web-react";
+import {
+  IconExclamationCircle,
+  IconLoading,
+} from "@arco-design/web-react/icon";
+import { useEffect, useRef } from "react";
 
-export default ({ error, name = "", id }) => {
+export default ({ error, name = "", id, resetErrorBoundary }) => {
+  const timer = useRef(null);
+
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      resetErrorBoundary?.();
+    }, 3000);
+    return () => clearInterval(timer.current);
+  }, []);
+
   return (
     <>
       {error ? (
@@ -13,6 +26,11 @@ export default ({ error, name = "", id }) => {
               {id && <span>组件id: {id}</span>}
               <br />
               <span>错误信息: {error.message}</span>
+              <br />
+              <Space>
+                <IconLoading />
+                正在尝试重新加载...
+              </Space>
             </>
           }
         />
